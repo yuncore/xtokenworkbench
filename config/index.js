1bc9b42716933c3704486f495c473d7b65411e74
lyping_1756 <lyping_1756@163.com>
2018-07-25 15:22:21 +0800

    test

--- config/index.js
+++ config/index.js
@@ -13,8 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '127.0.0.1', // can be overwritten by process.env.HOST
-    port: 5011, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host: '192.168.1.111', // can be overwritten by process.env.HOST
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
